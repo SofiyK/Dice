78142f9ad6869b92161d00ca1341e760c2edd0cd
SofiyK <sofiy.kalenichenko@gmail.com>
2025-03-17 00:03:33 +0300

    переписаны функции, переделан html документ и полностью переделан css стиль

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ function randomInteger() {
 };
 
 
-// функиция, удаляющая элементы перед созданием новых
-function clearWindow(...args) {
-    if (document.getElementById(args[0])){
-        for (let i = 0; i < args.length; i++) {
-            document.getElementById(args[i]).remove();
-        }
-    }
-};
+function updateContent() {
+    let pNum = randomInteger();
+    let cNum = randomInteger();
+
+    // Обновляем числа игрока и компьютера
+    document.getElementById('player').textContent = pNum;
+    document.getElementById('comp').textContent = cNum;
+
+    // Обновляем результат игры
+    document.getElementById('result').textContent = winner(pNum, cNum);
+}
 
 // функкция, высчитывающая победителя
 function winner(p, c) {
@@ -26,26 +29,13 @@ function winner(p, c) {
     }
 }
 
-// фнкция по созданию элементов с числами
-function create(elemN ,idN, classN, text) {
-    let created = document.createElement(elemN);
-    created.id = idN;
-    created.className = classN;
-    created.innerHTML = text;
-    document.body.append(created);
-};
 
 class Menu{
     constructor(elem) {
         elem.onclick = this.onClick.bind(this);
     }
-    start(){
-        clearWindow('player', 'comp', 'win');
-        let pNum = randomInteger();
-        let cNum = randomInteger();  
-        create('div', 'player', 'numbers', pNum);
-        create('div', 'comp', 'numbers', cNum);
-        create('div', 'win', 'text', winner(pNum, cNum));
+    start() {
+        updateContent(); // Обновляем содержимое вместо удаления и создания элементов
     }
     onClick(event) {
         let action = event.target.dataset.action;
